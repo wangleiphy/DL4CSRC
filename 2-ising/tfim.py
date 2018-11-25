5c4014ac69eb8dd4d1273ba4070c2e753ad7c99c
Lei Wang <wangleiphy@gmail.com>
2018-11-25 14:47:41 +0800

    fidelity as a loss

--- 2-ising/tfim.py
+++ 2-ising/tfim.py
@@ -1,4 +1,5 @@
 import torch
+torch.manual_seed(42)
 import torch.nn as nn
 import numpy as np 
 
@@ -15,6 +16,9 @@ def bflip(i,p):
     return i ^(1<<p)
 
 class TFIM(nn.Module):
+    '''
+    1D transverse field Ising model 
+    '''
     def __init__(self, L):
         super(TFIM, self).__init__()
 
@@ -28,8 +32,8 @@ class TFIM(nn.Module):
         # loop over all basis states
         for i in range(Nstates):
             #diagonal term
-            for si in range(self.L):
-                H[i,i] += -self.couplings[si]* (2*bget(i, si)-1) * (2*bget(i, si+1)-1)
+            for site in range(self.L):
+                H[i,i] += -self.couplings[site]* (2*bget(i, site)-1) * (2*bget(i, site+1)-1)
             
             #off-diagonal term
             for site in range(self.L):
@@ -38,20 +42,22 @@ class TFIM(nn.Module):
         return H
 
     def overlap(self, psi):
+        '''
+        overlap of the ground state with the given state (assuming normalized)
+        '''
         w, v = torch.symeig(self._buildH(), eigenvectors=True)
-        return (v[0, :] * psi).abs().sum()
-
+        return (v[:, 0] * psi).sum().abs()
 
 if __name__=='__main__':
     import sys 
-    L = 8  
+    L = 4
     model = TFIM(L)
 
     # a normalized target state 
     target = torch.randn(1<<L)
     target /= target.norm() 
 
-    optimizer = torch.optim.Adam(model.parameters(), lr = 1e-2)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 1e-1)
 
     params = list(model.parameters())
     params = list(filter(lambda p: p.requires_grad, params))
@@ -59,11 +65,9 @@ if __name__=='__main__':
     print ('total nubmer of trainable parameters:', nparams)
     
     for e in range(100):
-        loss = model.overlap(target)
+        loss = 1.-model.overlap(target) # infidelity
         
         model.zero_grad()
         loss.backward()
         optimizer.step()
         print (e, loss.item())
-
- 
